6ac16d7850106d240cf69b5f0d174810fc9937c0
lukecode <lucassocorrosilva@gmail.com>
2022-10-31 21:30:02 -0300

    Projeto em desenvolvimento

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  import Character from "./lib/Character.svelte";
+  let characters = [];
+  let page = 0;
+
+  async function loadCharacters() {
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character?page=${page}`
+    );
+    const data = await response.json();
+    characters = data.results;
+  }
+  loadCharacters();
+
+  function nextPage() {
+    page++;
+    loadCharacters();
+  }
+
+  function prevPage() {
+    page--;
+    loadCharacters();
+  }
+</script>
+
+<h1>Rick and Morty Svelte</h1>
+
+<div>
+  {#each characters as character}
+    <Character {character} />
+  {/each}
+</div>
+<div>
+  <button on:click={() => prevPage()} disabled={page === 1}>Voltar</button>
+  <button on:click={() => nextPage()}>Próximo</button>
+</div>
---
+++ src/lib/Character.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let character;
+</script>
+
+<section>
+  <img src={character.image} alt={character.name} />
+  <h1>{character.name}</h1>
+  <h3>{character.species}</h3>
+</section>
